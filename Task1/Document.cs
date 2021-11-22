6800a889e54ce2995717af62e19e962d531edf41
Oleksandr Korol <koros.korol2003@gmail.com>
2021-11-22 12:40:40 +0200

    Complete Task1

---
+++ Task1/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public abstract class Document
+    {
+        public abstract string DocumentName { get; protected set; }
+
+        public abstract void Create(string path);
+        public abstract void Open(string path);
+        public abstract void Edit(string line);
+    }
+}
---
+++ Task1/Txt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Txt : Document
+    {
+        public override string DocumentName { get; protected set; }
+        public override void Create(string path) { }
+        public override void Open(string path) { }
+        public override void Edit(string line) { }
+    }
+}
